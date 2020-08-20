2be805d9e36b71e6eeeced1d12390f4b6ce2a625
zanzanzanm03 <olafminister12365@hotmail.com>
2020-08-20 13:03:12 +0700

    add

--- shirtProject/Screen/drawerScreens/SearchScreen.js
+++ shirtProject/Screen/drawerScreens/SearchScreen.js
@@ -1,122 +1,60 @@
+//This is an example code to understand Slider// 
 
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
+//import react in our code. 
 
-export default class SearchScreen extends Component {
+//For React Native >= 0.59
+import Slider from '@react-native-community/slider'
+
+import { View, Text, StyleSheet,ImageBackground,Image} from 'react-native';
+
+export default class App extends Component {
+   constructor(props) {
+   super(props);
+   this.state = {
+     //Initial Value of slider
+     sliderValue: 15
+   };
+ }
 
   render() {
     return (
+      <ImageBackground source={require('D:/application/Mobileapp2/shirtProject/Image/back.jpg')}
+      style={{flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"}} 
+      >
       <View style={styles.container}>
-          <View style={styles.header}>
-            <View style={styles.headerContent}>
-                <Image style={styles.avatar} source={require('../image/profile.png')}/>
-                <Text style={styles.name}>
-                  Emma cloude
-                </Text>
-            </View>
-          </View>
-
-          <View style={styles.profileDetail}>
-            <View style={styles.detailContent}>
-              <Text style={styles.title}>Photos</Text>
-              <Text style={styles.count}>319</Text>
-            </View>
-            <View style={styles.detailContent}>
-              <Text style={styles.title}>Followers</Text>
-              <Text style={styles.count}>600</Text>
-            </View>
-            <View style={styles.detailContent}>
-              <Text style={styles.title}>Following</Text>
-              <Text style={styles.count}>500</Text>
-            </View>
-          </View>
-
-          <View style={styles.body}>
-            <View style={styles.bodyContent}>
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text fontSize = '50px'>ProFiles</Text>  
-              </TouchableOpacity> 
-              <Text style={styles.description}></Text>
-            </View>
-        </View>
+      <Image
+        source={require('D:/application/Mobileapp2/shirtProject/Image/aboutreact.png')}
+        style={styles.imageIt}
+      />
+        {/*Text to show slider value*/}
+        <Text style={{color: 'black'}}>Pricre :  {this.state.sliderValue}</Text>
+      
+        {/*Slider with max, min, step and initial value*/}
+        <Slider 
+          maximumValue={1000}
+          minimumValue={1}
+          minimumTrackTintColor="#307ecc"
+          maximumTrackTintColor="#000000"
+          step={1} 
+          value={this.state.sliderValue}
+          onValueChange={(sliderValue) => this.setState({ sliderValue })}
+        />
       </View>
+      </ImageBackground>
     );
   }
 }
-
 const styles = StyleSheet.create({
-  header:{
-    backgroundColor: "#696969",
-  },
-  headerContent:{
-    padding:30,
-    alignItems: 'center',
-  },
-  avatar: {
-    width: 130,
-    height: 130,
-    borderRadius: 63,
-    borderWidth: 4,
-    borderColor: "white",
-    marginBottom:10,
-  },
-  name:{
-    fontSize:22,
-    color:"#FFFFFF",
-    fontWeight:'600',
-  },
-  profileDetail:{
-    alignSelf: 'center',
-    marginTop:200,
-    alignItems: 'center',
-    flexDirection: 'row',
-    position:'absolute',
-    backgroundColor: "#ffffff"
-  },
-  detailContent:{
-    margin:10,
-    alignItems: 'center'
-  },
-  title:{
-    fontSize:20,
-    color: "#00CED1"
-  },
-  count:{
-    fontSize:18,
-  },
-  bodyContent: {
-    flex: 1,
-    alignItems: 'center',
-    padding:30,
-    marginTop:40
-  },
-  textInfo:{
-    fontSize:18,
-    marginTop:20,
-    color: "#696969",
-  },
-  buttonContainer: {
-    marginTop:10,
-    height:45,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
-    backgroundColor: "#696969",
-  },
-  description:{
-    fontSize:20,
-    color: "#00CED1",
-    marginTop:10,
-    textAlign: 'center'
-  },
+ container: {
+   flex: 1,
+   padding:20,
+   justifyContent: 'center',
+   backgroundColor: '#ecf0f1',
+ },
+ imageIt: {
+    justifyContent: 'center'
+ }
 });
- 
